e9c1ed133b6fc4a669d87f594b5f6144a44492cd
DIRECT0R-ctrl <DominantVortex@proton.me>
2025-12-24 09:28:24 +0100

    project court class (projectCourt.php)

---
+++ src/entity/projetCourt.php
@@ -0,0 +1,21 @@
+<?php
+
+class ProjetCourt extends Projet
+{
+    private int $duree;
+
+    public function __construct(string $titre, int $membreId, int $duree)
+    {
+        parent::__construct($titre, $membreId);
+        $this->setDuree($duree);
+    }
+
+    public function setDuree(int $duree): void
+    {
+        if ($duree <= 0) {
+            throw new InvalidArgumentException("Durée invalide");
+        }
+        $this->duree = $duree;
+    }
+}
+
