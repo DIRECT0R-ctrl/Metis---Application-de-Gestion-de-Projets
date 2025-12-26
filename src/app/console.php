d4a2040b1609981e5a698d1d54eafba4e350826c
DIRECT0R-ctrl <DominantVortex@proton.me>
2025-12-26 11:08:24 +0100

    initialisation of the app (src/app/console.php)

---
+++ src/app/console.php
@@ -0,0 +1,9 @@
+i<?php
+
+require_once __DIR__ . '/../repository/memberRepository.php';
+require_once __DIR__ . '/../entity/member.php';
+
+echo "=== METIS - Gestion des Membres ===\n";
+echo "1. Ajouter un membre\n";
+echo "2. Lister les membres\n";
+echo "Choix: ";
